da6b09dc6782c3f56d874bf545572d41cd964f7c
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-02 15:57:12 +0100

    navbar's array written

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,41 @@ export default {
     HeaderComp,
     MainComp,
     FooterComp
-  }
+  },
+  data(){
+    return{
+
+      //Array containing navbar's labels and links
+      navbarArray: [
+        {
+          label: 'Courses',
+          links: [],
+        },
+        {
+          label: 'Course Formats',
+          links: [
+            'Default Style', 'Classic', 'Udemy Affiliate Style', 'Offline Style', 'Free Courses', 'Lesson Style 1', 'Lesson Style 2'
+          ],
+        },
+        {
+          label: 'Add Course',
+          links: [],
+        },
+        {
+          label: 'Pages',
+          links: [
+            'About Us', 'Shop', 'Contact Us',
+          ],
+        },
+        {
+          label: 'Demos',
+          links: [
+            'Udemy Affiliate', 'Light LMS', 'Dark LMS', 'Offline Courses', 'Architecture', 'Online Accademy', 'Course Hub', 'Classic LMS', 'Private Instructor', 'RTL Demo', 'BuddyPress Demo', 'Distance Learning', 'Cooking Courses',
+          ],
+        },
+      ],
+    }
+  },
 }
 </script>
 
