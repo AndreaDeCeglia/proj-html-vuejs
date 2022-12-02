6d7b69526614e36c442650270cd68bf123fc79a0
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-02 15:17:50 +0100

    header,main and footer linked

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- here there are my 3 main components -->
+
+    <!-- HeaderComp -->
+    <HeaderComp/>
+
+    <!-- MainComp -->
+    <MainComp/>
+
+    <!-- FooterComp -->
+    <FooterComp/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue'
+import MainComp from './components/MainComp.vue'
+import FooterComp from './components/FooterComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp,
+    MainComp,
+    FooterComp
   }
 }
 </script>
@@ -22,7 +35,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+// import the fontawesome core /
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// import font awesome icon component /
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// add font awesome icon component 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
