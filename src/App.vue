c79c08bea691e8d456a8544960e2055f9b2864ef
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-05 11:13:35 +0100

    studentsArray written

--- src/App.vue
+++ src/App.vue
@@ -168,6 +168,24 @@ export default {
           what: 'Excellent Reviews',
         },
       ],
+
+      studentsArray: [
+        {
+          image: './assets/img/1-100x100.jpg',
+          phrase: 'Come as you are',
+          explanation: `MasterStudy used is an excellent workshop whether you come as a counselor, advisor, administrator, or faculty member. I am going home empowered. I am looking forward to attending the On Course MasterStudy and the MasterStudy National Conference next year and facilitating the use of this excellent retention/student success course at my college.`,
+        },
+        {
+          image: './assets/img/2-100x100.jpg',
+          phrase: 'Paints of the future',
+          explanation: `The response to your MasterStudy has been really overwhelming! Those who participated in the workshop are spreading the word here on campus and the “buzz” is on. The VP of Instruction wants you to come back! Her goal is to have more faculty trained. She also wants to attend a workshop herself. Our President told me Masterstudy needs to be the cornerstone of our success program.`,
+        },
+        {
+          image: './assets/img/4-100x100.jpg',
+          phrase: 'Investing for Your Future',
+          explanation: `It is no exaggeration to say this MasterStudy experience was transformative–both professionally and personally. This workshop will long remain a high point of my life. Thanks again…. I am feeling energized and eager to start teaching my class next week. I can’t wait to use all of my new teaching tools. I will absolutely recommend this workshop to other educators!`,
+        },
+      ],
     }
   },
 }
