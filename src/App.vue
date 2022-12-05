3f87861eafbe863554cd0c124c972f0ae5efec31
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-05 11:56:47 +0100

    all array written

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,7 @@ export default {
   data(){
     return{
 
+      // *********** header's arrays ************
       //Array containing navbar's labels and links
       navbarArray: [
         {
@@ -67,12 +68,13 @@ export default {
         'fa-brands fa-facebook',
         'fa-brands fa-twitter',
       ],
-
+      
+      // ************ main's arrays *************
       //Array containing new courses specificies
       //just photos still miss
       newCoursesArray: [
         {
-          image:'',
+          image:'photo-1496307042754-b4aa456c4a2d-370x200.jpeg',
           title: 'How to be a DJ? Make Electronic Music',
           type: 'Electronic',
           level: 'Advanced',
@@ -80,7 +82,7 @@ export default {
           duration: '20 hours',
         },
         {
-          image:'',
+          image:'photo-1491897554428-130a60dd4757-370x200.jpeg',
           title: 'Fashion Photography from professional',
           type: 'Fashion',
           level: 'Advanced',
@@ -88,7 +90,7 @@ export default {
           duration: '12 hours',
         },
         {
-          image:'',
+          image:'photo-missing1.jpeg',
           title: 'Graphic Design Basics Masterclass',
           type: 'Communication',
           level: 'Intermediate',
@@ -96,7 +98,7 @@ export default {
           duration: '10 hours',
         },
         {
-          image:'',
+          image:'cathryn-lavery-67852-unsplash-370x200.jpg',
           title: 'Interior design concepts Masterclass',
           type: 'Art',
           level: 'Intermediate',
@@ -104,7 +106,7 @@ export default {
           duration: '8 hours',
         },
         {
-          image:'',
+          image:'photo-1475452779376-caebfb988090-370x200.jpeg',
           title: 'Road Bike Manual or How to Be a Champion',
           type: 'Bicycling',
           level: 'Beginner',
@@ -112,7 +114,7 @@ export default {
           duration: '4 hours',
         },
         {
-          image:'',
+          image:'photo-missing2.jpg',
           title: 'Real Things Art Painting by Jason Ni',
           type: 'Art',
           level: 'Intermediate',
@@ -168,22 +170,53 @@ export default {
           what: 'Excellent Reviews',
         },
       ],
-
+      
+      //array containg students thoughts for slider
       studentsArray: [
         {
-          image: './assets/img/1-100x100.jpg',
+          image: '1-100x100.jpg',
           phrase: 'Come as you are',
           explanation: `MasterStudy used is an excellent workshop whether you come as a counselor, advisor, administrator, or faculty member. I am going home empowered. I am looking forward to attending the On Course MasterStudy and the MasterStudy National Conference next year and facilitating the use of this excellent retention/student success course at my college.`,
         },
         {
-          image: './assets/img/2-100x100.jpg',
+          image: '2-100x100.jpg',
           phrase: 'Paints of the future',
           explanation: `The response to your MasterStudy has been really overwhelming! Those who participated in the workshop are spreading the word here on campus and the “buzz” is on. The VP of Instruction wants you to come back! Her goal is to have more faculty trained. She also wants to attend a workshop herself. Our President told me Masterstudy needs to be the cornerstone of our success program.`,
         },
         {
-          image: './assets/img/4-100x100.jpg',
+          image: '4-100x100.jpg',
           phrase: 'Investing for Your Future',
           explanation: `It is no exaggeration to say this MasterStudy experience was transformative–both professionally and personally. This workshop will long remain a high point of my life. Thanks again…. I am feeling energized and eager to start teaching my class next week. I can’t wait to use all of my new teaching tools. I will absolutely recommend this workshop to other educators!`,
+        },
+      ],
+
+      //******* Footer's arrays ********* */
+      aboutObj: {
+        title: 'ABOUT',
+        text: 'Masterstudy is Education WordPress theme featured by Learning Management System (LMS) for online education. Developed by StylemixThemes.',
+      },
+
+      contactObj: {
+        address: 'USA, Callifornia 20, First Avenue, Callifornia',
+        tel: 'Tel.: +1 212 458 300 32',
+        fax: 'Fax: +1 212 375 24 14',
+        mail: 'info@masterstudy.com',
+      },
+
+      pagesArray: [
+        'Blog', 'Home', 'Typography', 'Courses', 'Shortcodes', 'Membership' 
+      ],
+
+      blogArray: [
+        {
+          image: 'photo-1517520287167-4bbf64a00d66-50x50.jpeg',
+          title: 'Our main target is to "Developing Yourself as a Leader"',
+          date: '-August9, 2018',
+        },
+        {
+          image: 'photo-1490376840453-5f616fbebe5b-50x50.jpeg',
+          title: `Those Other College Expenses You Aren't Thinking About`,
+          date: '-June3, 2015',
         },
       ],
     }
